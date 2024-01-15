73b706c5ae4711b797e28fa5731f0f6bd285bd9a
distro104 <distro104@gmail.com>
2024-01-15 18:37:49 -0300

    wip: api-client-simple

--- api-client-simple/api/index.php
+++ api-client-simple/api/index.php
@@ -1,49 +1,80 @@
 <?php
 
-$request = $_GET['param'];
-$data['status'] = "ERROR";
-$data['data'] = null;
+use LDAP\Result;
 
-switch ($request) {
-    case 'status':
-        define_response($data, 'API RUNING OK!...');
-        break;
+$data_api_request = $_REQUEST;
 
-    case 'getnumber':
-        define_response($data, get_random_number());
-        break;
+// NECESSARIO TRATAR O PROBLEMA SE FORNECIDO O VALOR QUE NAO EXISTE A VARIAVEL PASSADA (data_api_request['request'])!
+$data_api_response = isset($data_api_request['request']) <> null ? define_data_api_response($data_api_request) : get_fail_result();
+
+define_response($data_api_response);
+
+// ---------------------------------------------------------------------------------- //
+function define_data_api_response($data_api_request)
+{
+    $data = null;
+    switch ($data_api_request['request']) {
+        case 'status':
+            $data = get_data_status_api();
+            break;
+
+        case 'getnumber':
+            $data = get_random_number($data_api_request);
+            break;
+
+        default:
+            $data = get_fail_result();
+            break;
+    }
+    return $data;
 }
 
-// MOSTRA O RESULTADO DA BUSCA.
-response($data);
-
-function geraRand(int $qtd = 1)
+function get_data_status_api()
 {
-    $result = [];
-    
+    $result = [
+        'status' => "SUCCESS!",
+        'data' => null,
+    ];
+    return $result;
 }
 
-//
-function define_response(&$data, $value)
+function get_random_number($data_request)
 {
-    $data['status'] = 'SUCCESS!';
-    $data['data'] = $value;
+    if (!isset($data_request['min']) ||  !isset($data_request['max'])) {
+        $result = get_fail_result();
+        return $result;
+    }
+
+    $result = get_success_result();
+    array_push($result['data'], "GERANDO VALORES ALEATORIOS!!");
+    for ($i = 0; $i < 10; $i++) {
+        array_push($result['data'], rand($data_request['min'], $data_request['max']));
+    }
+    array_push($result['data'], "FINALIZADO!!!");
+
+    return $result;
+}
+
+function get_success_result()
+{
+    return $result = [
+        'status' => "SUCCESS!",
+        'data' => []
+    ];
+}
+
+function get_fail_result()
+{
+    return $result = [
+        'status' => "FAIL REQUEST!...!",
+        'data' => null
+    ];
 }
 
 // CONSTRUCAO DA RESPONSE
-function response($data_response)
+function define_response($api_data_response)
 {
     header("Content-Type:application/json");
-    echo json_encode($data_response);
-}
-
-function get_random_number($value = 10)
-{
-    $number = [];
-    array_push($number, "GERANDO VALORES ALEATORIOS!!");
-    for ($i=0; $i < $value; $i++) {
-        array_push($number, rand(1, 100));
-    }
-    array_push($number, "FINALIZADO!!!");
-    return $number;
+    echo json_encode($api_data_response);
+    // var_dump($api_data_response);
 }
