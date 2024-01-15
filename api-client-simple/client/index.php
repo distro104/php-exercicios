c55b2288fafe5a29564789fe9ae433fe2f8d8502
distro104 <distro104@gmail.com>
2024-01-15 19:18:42 -0300

    wip: client-api

--- api-client-simple/client/index.php
+++ api-client-simple/client/index.php
@@ -1,29 +1,23 @@
 <?php
 
-define('API_BASE','http://localhost/php-exercicios/api-client-simple/api/?param=getnumber');
+define('API_BASE_URL','http://localhost/php-exercicios/api-client-simple/api/?request=getnumber&min=1&max=100');
 
 echo '<h3>APLICACAO<h3><hr />';
 
-$resultado = api_request();
-
+$resultado = api_data_request();
 var_dump($resultado);die();
-
 if($resultado['status']=='ERROR'){ die('Ocorreu um erro'); }
 
 echo '<pre>';
 print_r($resultado);
 echo '</pre>';
 
-function api_request()
+function api_data_request()
 {
-    $client = curl_init();
-
-    curl_setopt($client, CURLOPT_URL, API_BASE);
-
-    $response = curl_exec($client);
-
-    curl_close($client);
+    $api_server = curl_init(API_BASE_URL);
+    curl_setopt($api_server, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($api_server);
 
     // return json_decode($response);
-    return $response;
+    return json_decode($response, true);
 }
