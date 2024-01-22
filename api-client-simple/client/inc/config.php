ccdb43fd3c8a310d93815872e110f2036cfcbd25
distro104 <distro104@gmail.com>
2024-01-22 14:17:05 -0300

    wip: epi client

---
+++ api-client-simple/client/inc/config.php
@@ -0,0 +1,4 @@
+<?php
+
+// DEFINE URls
+define('API_BASE_URL','http://localhost/php-exercicios/api-client-simple/api/');
--- api-client-simple/client/index.php
+++ api-client-simple/client/index.php
@@ -1,23 +1,33 @@
 <?php
 
-define('API_BASE_URL','http://localhost/php-exercicios/api-client-simple/api/?request=getnumber&min=1&max=100');
+// define('API_BASE_URL','http://localhost/php-exercicios/api-client-simple/api/?request=getnumber&min=1&max=100');
 
-echo '<h3>APLICACAO<h3><hr />';
+// echo '<h3>APLICACAO<h3><hr />';
 
-$resultado = api_data_request();
+// $resultado = api_data_request();
 
-if($resultado['status']=='ERROR'){ die('Ocorreu um erro'); }
+// if($resultado['status']=='ERROR'){ die('Ocorreu um erro'); }
 
-echo '<pre>';
-print_r($resultado);
-echo '</pre>';
+// echo '<pre>';
+// print_r($resultado);
+// echo '</pre>';
 
-function api_data_request()
-{
-    $api_server = curl_init(API_BASE_URL);
-    curl_setopt($api_server, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($api_server);
+// function api_data_request()
+// {
+//     $api_server = curl_init(API_BASE_URL);
+//     curl_setopt($api_server, CURLOPT_RETURNTRANSFER, true);
+//     $response = curl_exec($api_server);
 
-    // return json_decode($response);
-    return json_decode($response, true);
-}
+//     // return json_decode($response);
+//     return json_decode($response, true);
+// }
+
+require_once('inc/config.php');
+require_once('inc/api_function.php');
+
+$variables = [
+    'nome' => 'Otavio',
+    'apelido' => 'TROUXA'
+];
+
+api_request('status', 'GET', $variables);
